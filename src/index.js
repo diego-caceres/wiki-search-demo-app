2dfd8a2267c7bee45d2bb2cd67f6f71d4d306a8b
Diego Cáceres <dcaceres@xseed.com.uy>
2017-04-18 17:47:43 -0300

    Initial files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WikiSearch from './WikiSearch.js';
+import './index.css';
+
+
+ReactDOM.render(
+  <WikiSearch />,
+  document.getElementById('root')
+);
